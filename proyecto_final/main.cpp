682b8cd9551fa303eeb37a00c3875fa9a24c8315
Hiram Castillo <hiramcastillo36@gmail.com>
2023-11-25 20:45:10 -0600

    feat: code cleanup

--- proyecto_final/main.cpp
+++ proyecto_final/main.cpp
@@ -32,100 +32,13 @@ int main( void )
     glDepthFunc(GL_LESS);
 
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-/*
-    Model <Ply> ball_sim("models/rock.ply", 1.0, 0.0, 0.0);
-    Model <Ply> robot_sim("models/robot.ply", 1.0, 0.0, 1.0);
-    Model <Obj> enemy_sim("models/enemy2.obj", 0.0, 0.0, 1.0);
 
-	glClearColor(0.0f, 0.0f, 0.0f ,0.0f);
-	
-    srand (time(NULL));
-    float random_enemy = (float)(rand() % 2 + 1)/10;
-    cout << random_enemy << endl;
-
-    //Transformaciones
-    glm::mat4 scale_robot = glm::scale(glm::mat4(1.0f), glm::vec3(0.4f));
-    glm::mat4 translate_robot = glm::translate(glm::mat4(1.0f), glm::vec3(-1.0f,0.0f,0.0f));    
-    
-    // ball
-    glm::mat4 scale_enemy = glm::scale(glm::mat4(1.0f), glm::vec3(0.4f));
-    glm::mat4 translate_ball = glm::translate(glm::mat4(1.0f), glm::vec3(1.2f,0.0f,0.0f));
-
-    // enemy
-    //Transformation for ball
-    glm::mat4 scale_ball = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
-    glm::mat4 translate_enemy = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f,0.0f, random_enemy));
-    
-    Animation an;
-
-    Vertex P1(-1.0,0.0,0.0);
-    Vertex P2(-0.8,0.0,1.5);
-    Vertex P3(0.5,0.0,0.5);
-    Vertex P4(1.0,0.0,0.0);
-
-    vector<Vertex> robot_path = an.bezier(P1, P2, P3, P4, 0.009);
-
-    unsigned int star_position = 0;
-*/
-/*
-    // Desde arriba
-    glm::mat4 View = glm::lookAt(
-        glm::vec3(0,2,0), // Camera is at (0,0,2), in World Space
-        glm::vec3(0,0,0), // and looks at the origin
-        glm::vec3(0,0, -1)  // Head is up (set to 0,-1,0 to look upside-down)
-    );
-    // Desde abajo
-    
-    glm::mat4 View = glm::lookAt(
-        glm::vec3(2,1,1), // Camera is at (0,0,2), in World Space
-        glm::vec3(0,0,0), // and looks at the oribgin
-        glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-    );
-
-    glm::mat4 View = glm::lookAt(
-        glm::vec3(0,0,2), // Camera is at (0,0,2), in World Space
-        glm::vec3(0,0,0), // and looks at the origingggggggggg
-        glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-    );
-    */
-/*
-    glm::mat4 View = glm::lookAt(
-        glm::vec3(2,1,2), // Camera is at (0,0,2), in World Space
-        glm::vec3(-1,0,0), // and looks at the oribgin
-        glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-    );
-
-    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), (float) 1024 / (float) 500, 0.1f, 100.0f);
-
-    //Rotation for ball
-    
-    glm::mat4 transform_ball = translate_ball * scale_ball;
-*/
     Simulation simulation;
     do {
-        
-/*
-        glm::mat4 translate_robot = glm::translate(glm::mat4(1.0f), glm::vec3(robot_path[star_position].getX(), 0.0, robot_path[star_position].getZ()));
-        glm::mat4 transform_robot = translate_robot * scale_robot;
-
-        //cout<<robot_path[star_position].getX()<<" "<<robot_path[star_position].getY()<< " " << robot_path[star_position].getZ()<<endl;
-
-        if(star_position < robot_path.size() - 1)
-            star_position++;
-        else
-            star_position = 0;
-
-        // Clear the screen
-        
-        glm::mat4 mvp = Projection * View * translate_enemy * scale_enemy;
-        glm::mat4 mvp2 = Projection * View * transform_ball;
-        glm::mat4 mvp3 = Projection * View * transform_robot;
-        ball_sim.draw(gl.getProgramID(), mvp2);
-        robot_sim.draw(gl.getProgramID(), mvp3);
-        enemy_sim.draw(gl.getProgramID(), mvp);
-*/
         glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+        
         simulation.init(gl.getProgramID());
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
 
